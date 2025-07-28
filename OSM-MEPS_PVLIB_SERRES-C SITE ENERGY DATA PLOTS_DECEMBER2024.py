f389ca9ec41e114d01a0696de4e68462e012718c
PeterMunyao <155031612+PeterMunyao@users.noreply.github.com>
2025-07-28 18:51:03 +0200

    Update OSM-MEPS_PVLIB_SERRES-C SITE ENERGY DATA PLOTS_DECEMBER2024.py

--- OSM-MEPS_PVLIB_SERRES-C SITE ENERGY DATA PLOTS_DECEMBER2024.py
+++ OSM-MEPS_PVLIB_SERRES-C SITE ENERGY DATA PLOTS_DECEMBER2024.py
@@ -53,7 +53,8 @@ temp_cell = pvlib.temperature.sapm_cell(
     poa_irradiance, df['air_temp'], df['wind_speed_10m'], -3.47, -0.0594, 3
 )
 
-dc_power_pvlib = poa_irradiance * num_panels * 0.25 * (1 + temp_coeff_pvlib * (temp_cell - 25))
+#old line: dc_power_pvlib = poa_irradiance * num_panels * 0.25 * (1 + temp_coeff_pvlib * (temp_cell - 25)), units mismatch w/m2 for power (W)
+dc_power_pvlib = poa_irradiance / stc_irradiance * num_panels * panel_power_max * (1 + temp_coeff * (temp_cell - 25))
 ac_power_pvlib = dc_power_pvlib * inverter_efficiency_pvlib
 df['pvlib_energy_kWh'] = (ac_power_pvlib / 1000).resample('H').mean()
 daily_energy_pvlib = df['pvlib_energy_kWh'].resample('D').sum()
